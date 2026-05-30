b4bff339a2f85b5d21cc5e0623d19176e7540440
Marvyn <jim51507jim@gmail.com>
2026-05-30 22:16:14 +0800

    force with openmp

--- src/force.c
+++ src/force.c
@@ -117,3 +117,116 @@ void compute_force(SPHSystem2D *sph){
   return;
 }
 
+
+// make sure that this is always inline
+// inline: save time for jumping around different function part. 
+// static: only inside this file, while compiling, this is better for optimization
+// __attribute__((always_inline))
+// when compiling, compiler can discard this after compiling this part, and optimize it directly. 
+// inline is only a "suggestion", especially in O3, sometimes it changes idea, since this is a long function
+// however in our simulation, this need to be inline!
+__attribute__((always_inline)) static inline void compute_pairwise_physics(Particle *p_i, Particle *p_j, SPHSystem2D *sph) {
+    double dx = p_i->x - p_j->x;
+    double dy = p_i->y - p_j->y;
+    double r = sqrt(dx * dx + dy * dy);
+    if (r < 1e-12) return;  // avoid error
+
+    double max_h = fmax(p_i->h, p_j->h);
+    if (r > max_h) return;  // Viscosity & Force only active within max_h
+
+    double dvx = p_i->vx - p_j->vx;
+    double dvy = p_i->vy - p_j->vy;
+
+    // get the W_ij(h_i) and W_ij(h_j)
+    double W_i, dWdr_i, dWdh_i;
+    cubic_spline_kernel_2d(r, p_i->h, &W_i, &dWdr_i, &dWdh_i);
+    double W_j, dWdr_j, dWdh_j;
+    cubic_spline_kernel_2d(r, p_j->h, &W_j, &dWdr_j, &dWdh_j);
+
+    // --- 1. Pressure Force ---
+    double term_i = p_i->factor * p_i->pressure / (p_i->rho * p_i->rho) * dWdr_i;
+    double term_j = p_j->factor * p_j->pressure / (p_j->rho * p_j->rho) * dWdr_j;
+    double scalar_force = p_j->mass * (term_i + term_j);
+
+    double ax = -scalar_force * (dx/r);
+    double ay = -scalar_force * (dy/r);
+
+    p_i->ax += ax;
+    p_i->ay += ay;
+
+    // --- 2. Thermal Energy Evolution (Pressure Part) ---
+    double inner_product_v_dW_i = dvx * (dWdr_i * dx / r) + dvy * (dWdr_i * dy / r);
+    double inner_product_v_dW_j = dvx * (dWdr_j * dx / r) + dvy * (dWdr_j * dy / r); // 必須保留，avg_inner 會用到
+    
+    p_i->dudt += p_i->factor * p_i->pressure / (p_i->rho * p_i->rho) * p_j->mass * inner_product_v_dW_i;
+
+    // --- 3. Artificial Viscosity ---
+    double r_dot_v = dx * dvx + dy * dvy;
+    double Vax = 0.0, Vay = 0.0;
+    
+    if (r_dot_v < 0.0) {
+        double h_ij = (p_i->h + p_j->h) / 2.0;
+        double mu_ij = h_ij * r_dot_v / (r * r + sph->visc_epsilon * (h_ij * h_ij));
+        double c_ij = (p_i->cs + p_j->cs) / 2.0;
+        double rho_ij = (p_i->rho + p_j->rho) / 2.0;
+        double PI_ij = (-sph->visc_alpha * c_ij * mu_ij + sph->visc_beta * mu_ij * mu_ij) / rho_ij;
+
+        double avg_inner = (inner_product_v_dW_i + inner_product_v_dW_j) / 2.0;
+        p_i->dudt += p_j->mass / 2.0 * PI_ij * avg_inner;
+
+        Vax = -p_j->mass * PI_ij * ((dWdr_i + dWdr_j)/2.0 * dx/r);
+        Vay = -p_j->mass * PI_ij * ((dWdr_i + dWdr_j)/2.0 * dy/r);
+        p_i->ax += Vax;
+        p_i->ay += Vay;
+    }
+
+    // --- 4. Reaction force only for single core
+#ifndef OMP
+    double mass_ratio = p_i->mass / p_j->mass;
+    
+    p_j->ax -= (ax + Vax) * mass_ratio;
+    p_j->ay -= (ay + Vay) * mass_ratio;
+    p_j->dudt += p_j->factor * p_j->pressure / (p_j->rho * p_j->rho) * p_i->mass * inner_product_v_dW_j;
+    
+    if (r_dot_v < 0.0) {
+        double avg_inner = (inner_product_v_dW_i + inner_product_v_dW_j) / 2.0;
+        p_j->dudt += p_i->mass / 2.0 * PI_ij * avg_inner;
+    }
+#endif
+}
+
+
+void compute_force(SPHSystem2D *sph) {
+#ifdef OMP
+#pragma omp parallel for
+#endif
+  // initialize the ax,ay,dudt in every particle
+  for (int i = 0; i < sph->N; i++){
+    sph->particles[i].ax = 0.0;
+    sph->particles[i].ay = 0.0;
+    sph->particles[i].dudt = 0.0;
+  }
+
+#ifdef OMP
+#pragma omp parallel for
+    // openmp, run every i to j, for parallelization
+    for (int i = 0; i < sph->N; i++) {
+        Particle *p_i = &sph->particles[i];
+        for (int j = 0; j < sph->N; j++) {
+            if (i == j) continue;
+            Particle *p_j = &sph->particles[j];
+            compute_pairwise_physics(p_i, p_j, sph);
+        }
+    }
+#else
+    // run only pairs (half the number of the computation)
+    for (int i = 0; i < sph->N; i++) {
+        Particle *p_i = &sph->particles[i];
+        for (int j = i + 1; j < sph->N; j++) {
+            Particle *p_j = &sph->particles[j];
+            compute_pairwise_physics(p_i, p_j, sph);
+        }
+    }
+#endif
+
+}
